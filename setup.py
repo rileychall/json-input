9b7fceda7b56888d9161c68f27c90bb61b571b51
Riley Hall <riley.hall.va@gmail.com>
2020-11-24 15:54:42 -0500

    Finish prep for PyPI upload

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import setuptools
-from setuptools import version
+from pathlib import Path
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -13,11 +13,14 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rileychall/json-input",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[Path(path).stem() for path in Path(".").glob("src/*.py")],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
+    install_requires=["jsonschema"],
 )
